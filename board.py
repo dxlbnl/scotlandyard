a086c276deb0ee5ee1dfd7af6d9bcb0797bc6102
Dexter <a.esselink@gmail.com>
2013-01-09 07:22:54 -0500

    Moved the player specifics to another file, started defining the model

--- board.py
+++ board.py
@@ -6,50 +6,12 @@
 	Beginnings:
 	    there is 1 mrX, and several detectives.
 	    every player (mrX and the detectives) gets a starting position. 
-
-
+        
 """
 
 import json
 
-class Player(object):
-	identifiers = 0
-
-	position = 0
-
-	def __init__(self):
-		self.id = "{}-{}".format(self.__class__.__name__, self.identifiers)
-		Player.identifiers += 1
-		self.tickets = self.tickets.copy()
-
-	def __repr__(self):
-		return self.id
-
-	def setup(self, board, game):
-		print "Setting board", self, board, game
-		self.board = board
-		self.game  = game
-
-	def set_position(self, position):
-		self.position = position
-
-class Detective(Player):
-	# initial tickets, will get copied (shallow)
-	tickets = {
-		'taxi'  : 10,
-		'bus'   : 8,
-		'metro' : 4
-	}
-
-class MrX(Player):
-	# initial tickets, will get copied (shallow)
-	tickets = {
-		'taxi'   : 4,
-		'bus'    : 3,
-		'metro'  : 3,
-		'double' : 2
-	}
-
+from player import MrX, Detective
 
 class Board(object):
 	stops = []
